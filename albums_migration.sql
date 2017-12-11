be3352b2dbfd86d5932e150f3ab89b6d3334b5a4
abbyperez <abbyperez28@gmail.com>
2017-12-11 11:52:23 -0600

    Completed MySQL insert exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,1 +1,13 @@
-test
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+create table albums (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  artist_name VARCHAR(50),
+  record_name VARCHAR(100) NOT NULL,
+  release_date INT,
+  sales FLOAT,
+  genre VARCHAR(75),
+  primary key (id)
+);

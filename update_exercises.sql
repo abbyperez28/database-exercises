b3c52d06c594cb54562b7aef9c638cec91c1c083
abbyperez <abbyperez28@gmail.com>
2017-12-11 14:31:19 -0600

    solve update exercise

---
+++ update_exercises.sql
@@ -0,0 +1,20 @@
+USE codeup_test_db;
+
+-- SELECT *
+-- FROM albums;
+--
+-- SELECT *
+-- FROM albums
+-- WHERE release_date < 1980;
+--
+-- SELECT * FROM albums where artists = 'Michael Jackson';
+
+UPDATE albums SET sales = sales * 10;
+
+UPDATE albums
+  SET release_date = release_date - 100
+WHERE release_date < 1980;
+
+UPDATE albums
+  SET artist = 'Peter Jackson'
+WHERE artisit = 'Michael Jackson';

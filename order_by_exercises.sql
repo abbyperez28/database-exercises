9f67286bc5cf2580b7ee06a5df28f7d2982510f0
abbyperez <abbyperez28@gmail.com>
2017-12-13 10:15:08 -0600

    functions solutions

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -3,10 +3,10 @@ use employees;
 # //Modify your first query to order by first name.
 # //The first result should be Irena Majewski and the last result should be Vidya Schaft.
 
-SELECT *
+SELECT first_name, last_name
 FROM employees
 WHERE first_name IN ('Irena', 'Vidya', 'Maya')
-ORDER BY first_name;
+ORDER BY first_name, last_name;
 
 
 # //Update the query to order by first name and then last name.
@@ -33,7 +33,16 @@ ORDER BY last_name, first_name;
 SELECT last_name, emp_no
 FROM employees
 WHERE last_name LIKE 'E%'
-ORDER BY emp_no;
+ORDER BY emp_no DESC;
+
+
+# Change the query for employees hired in the 90s and born on Christmas such that the first
+# result is the oldest employee who was hired last. It should be Khun Bernini.
+
+SELECT *
+FROM employees
+WHERE hire_date like '199%' AND birth_date like '%-12-25'
+ORDER BY birth_date ASC, hire_date DESC;
 
 
 
